89113e26142794a3ec3a279dda9a013ad862ee12
GANDHEEPKUMAR_git <gandheepkumarbokka@gmail.com>
2025-10-05 17:43:49 +0530

    deleted

--- FINANCE/src/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-// Utility to format numbers as Indian Rupees
-export function formatRupees(amount) {
-  return amount.toLocaleString('en-IN', {
-    style: 'currency',
-    currency: 'INR',
-    maximumFractionDigits: 2,
-  });
-}
