83a1868e2419b376a9a1b7061488ffc11a799f90
GANDHEEPKUMAR_git <gandheepkumarbokka@gmail.com>
2025-10-05 16:36:29 +0530

    Initial commit: Adding portfolio and other HTML/CSS/JS files

---
+++ HTML_CSS_JS/script1.js
@@ -0,0 +1,10 @@
+let selectorsection = document.getElementById("selectors");
+console.log(selectorsection);
+let boxmodelsection = document.querySelector("#boxmodel");
+console.log(boxmodelsection);
+let button1 = document.getElementById("button1");
+console.log(button1);
+button1.addEventListener("click",function(){
+    console.log("Button 1 clicked");
+    alert("Button 1 was clicked!");
+});
